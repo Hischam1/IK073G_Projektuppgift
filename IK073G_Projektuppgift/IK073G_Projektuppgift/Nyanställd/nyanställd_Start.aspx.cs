2a14ceebc4e60d69230d1c3190c092923a3f3f6b
Hischam El Yahiaoui <hiel1500@student.miun.se>
2016-10-31 13:30:50 +0100

    a

--- IK073G_Projektuppgift/IK073G_Projektuppgift/Nyanställd/nyanställd_Start.aspx.cs
+++ IK073G_Projektuppgift/IK073G_Projektuppgift/Nyanställd/nyanställd_Start.aspx.cs
@@ -15,6 +15,9 @@ namespace IK073G_Projektuppgift
         List<QA> Kategori1QALista = new List<QA>();
         List<QA> Kategori2QALista = new List<QA>();
         List<QA> Kategori3QALista = new List<QA>();
+        //QA qa = new QA();
+        List<QA> haa;
+        List<QA> hej;
 
         string valtNamn;
         Postgres p = new Postgres();
@@ -25,21 +28,20 @@ namespace IK073G_Projektuppgift
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            nyanställningsLista.DataSource = p.HämtaNyanställda();
-            nyanställningsLista.DataBind();
-           
+
+                nyanställningsLista.DataSource = p.HämtaNyanställda();
+                nyanställningsLista.DataBind();
+
+            xmlto
+            hej = XmlTillLista();
         }
         public void VisaAllt(List<QA> QALista)
         {
             Random r = new Random();
             var blandadLista = QALista.OrderBy(c => r.Next()).ToList();
-                        
+
             foreach (QA qa in blandadLista)
             {
-
-                //HtmlGenericControl divBild = new HtmlGenericControl("div class=bild");
-                //divBild.InnerText = qa.bild;
-                //bild.Controls.Add(divBild);
 
                 HtmlGenericControl divFråga = new HtmlGenericControl("div class=fråga");
                 frågeform.Controls.Add(divFråga);
@@ -104,6 +106,7 @@ namespace IK073G_Projektuppgift
             Kategori3QALista.Clear();
 
 
+
             string path = Server.MapPath("../Q&A.xml");
             XmlDocument doc = new XmlDocument();
             doc.Load(path);
@@ -141,6 +144,7 @@ namespace IK073G_Projektuppgift
                         Kategori1QALista.Add(AllaQALista[i]);
                     }
                 }
+                
 
                 return Kategori1QALista;
             }
@@ -174,28 +178,129 @@ namespace IK073G_Projektuppgift
             }
         }
 
+        public void VisaFråga(QA qa)
+        {
+            HtmlGenericControl divFråga = new HtmlGenericControl("div class=fråga");
+            frågeform.Controls.Add(divFråga);
+
+            HtmlGenericControl kategoriFråga = new HtmlGenericControl("p class=frågaKategori");
+            kategoriFråga.InnerText = qa.kategori;
+            divFråga.Controls.Add(kategoriFråga);
+
+            HtmlGenericControl bildFråga = new HtmlGenericControl("img src= '" + qa.bild + "' class=bildFråga width=20%");
+            divFråga.Controls.Add(bildFråga);
+
+            HtmlGenericControl rubrikFråga = new HtmlGenericControl("p class=frågaRubrik id=hej runat=server");
+            rubrikFråga.InnerText = qa.fråga;
+            divFråga.Controls.Add(rubrikFråga);
+
+            HtmlGenericControl divText = new HtmlGenericControl("div class=text");
+            divText.InnerText = qa.text;
+            divFråga.Controls.Add(divText);
+
+            HtmlGenericControl svarsalternativDivFråga = new HtmlGenericControl("div class=svarsalternativ");
+            divFråga.Controls.Add(svarsalternativDivFråga);
+
+            HtmlGenericControl svar1Fråga = new HtmlGenericControl("li class=svar1");
+            svarsalternativDivFråga.Controls.Add(svar1Fråga);
+            HtmlGenericControl checkBox1Fråga = new HtmlGenericControl("input type = checkbox name = svar1");
+            svar1Fråga.Controls.Add(checkBox1Fråga);
+            HtmlGenericControl svar1TextFråga = new HtmlGenericControl("p");
+            svar1TextFråga.InnerText = qa.svar1;
+            svar1Fråga.Controls.Add(svar1TextFråga);
+
+            HtmlGenericControl svar2Fråga = new HtmlGenericControl("li class=svar2");
+            svarsalternativDivFråga.Controls.Add(svar2Fråga);
+            HtmlGenericControl checkBox2Fråga = new HtmlGenericControl("input type = checkbox name = svar2");
+            svar2Fråga.Controls.Add(checkBox2Fråga);
+            HtmlGenericControl svar2TextFråga = new HtmlGenericControl("p");
+            svar2TextFråga.InnerText = qa.svar2;
+            svar2Fråga.Controls.Add(svar2TextFråga);
+
+            HtmlGenericControl svar3Fråga = new HtmlGenericControl("li class=svar3");
+            svarsalternativDivFråga.Controls.Add(svar3Fråga);
+            HtmlGenericControl checkBox3Fråga = new HtmlGenericControl("input type = checkbox name = svar3");
+            svar3Fråga.Controls.Add(checkBox3Fråga);
+            HtmlGenericControl svar3TextFråga = new HtmlGenericControl("p");
+            svar3TextFråga.InnerText = qa.svar3;
+            svar3Fråga.Controls.Add(svar3TextFråga);
+
+            HtmlGenericControl svar4Fråga = new HtmlGenericControl("li class=svar4");
+            svarsalternativDivFråga.Controls.Add(svar4Fråga);
+            HtmlGenericControl checkBox4Fråga = new HtmlGenericControl("input type = checkbox name = svar4");
+            svar4Fråga.Controls.Add(checkBox4Fråga);
+            HtmlGenericControl svar4TextFråga = new HtmlGenericControl("p");
+            svar4TextFråga.InnerText = qa.svar4;
+            svar4Fråga.Controls.Add(svar4TextFråga);
+
+        }
+        public QA XmlTillFråga()
+        {
+
+            string path = Server.MapPath("../Q&A.xml");
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+
+            XmlNodeList allaFrågorOchSvar = doc.SelectNodes("/bank/Frågor/FrågorLicensiering/Frågenummer");
+            QA qa = new QA();
+
+            foreach (XmlNode node in allaFrågorOchSvar)
+            {
+
+                qa.kategori = node["Kategori"].InnerXml;
+                qa.typ = node["Typ"].InnerXml;
+                qa.text = node["Text"].InnerXml;
+                qa.bild = node["Bild"].InnerXml;
+                qa.fråga = node["Fråga"].InnerXml;
+                qa.svar1 = node["Svar1"].InnerXml;
+                qa.svar2 = node["Svar2"].InnerXml;
+                qa.svar3 = node["Svar3"].InnerXml;
+                qa.svar4 = node["Svar4"].InnerXml;
+                qa.rättSvar1 = node["RättSvar1"].InnerXml;
+                qa.rättSvar2 = node["RättSvar2"].InnerXml;
+                qa.rättSvar3 = node["RättSvar3"].InnerXml;
+
+            }
+            return qa;
+        }
+
+
 
 
         protected void startaNyttTest_Click(object sender, EventArgs e)
         {
+
             kategori1 = true;
             kategori2 = false;
             kategori3 = false;
-
             
+
             nästaSida1.Visible = true;
             provText.Visible = false;
-            startaNyttTest.Visible = false;
+            //startaNyttTest.Visible = false;
             nyanställningsLista.Visible = false;
             namnet.InnerHtml = nyanställningsLista.SelectedItem.Value;
 
-            VisaAllt(XmlTillLista());
+            if (IsPostBack)
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    VisaFråga(hej[i]);
+                    hej.RemoveAt(i);
+                }
+
+            }
+
+
+
+
+
+            //VisaFåga(XmlTillFråga());
+
         }
 
         protected void nästaSida1_Click(object sender, EventArgs e)
         {
-            
-            
 
             kategori1 = false;
             kategori2 = true;
@@ -205,6 +310,8 @@ namespace IK073G_Projektuppgift
             nästaSida2.Visible = true;
 
             VisaAllt(XmlTillLista());
+            //VisaFåga(XmlTillFråga());
+
         }
 
         protected void nästaSida2_Click(object sender, EventArgs e)
@@ -217,6 +324,7 @@ namespace IK073G_Projektuppgift
             avslutaProv.Visible = true;
 
             VisaAllt(XmlTillLista());
+            //VisaFåga(XmlTillFråga());
         }
 
         protected void avslutaProv_Click(object sender, EventArgs e)
