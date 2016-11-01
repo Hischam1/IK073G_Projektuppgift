cf14f0d88e63555122020734be6575e1ea607a59
Johan Conradsson <conradsson1993@hotmail.com>
2016-11-01 11:29:40 +0100

    blabla

--- IK073G_Projektuppgift/IK073G_Projektuppgift/Nyanställd/nyanställd_Start.aspx
+++ IK073G_Projektuppgift/IK073G_Projektuppgift/Nyanställd/nyanställd_Start.aspx
@@ -6,7 +6,6 @@
                     <%--<a href ="Index.aspx">--%><img id="logotyp" src="/Images/logga.gif" alt="JE LOGO"/><%--</a>--%>
 
                 <div class="nav">
-                    <div id="hej" runat="server">aa</div>
 				        <ul id="navRutor">
 					        <li ><a href="#" class="current">Prov</a></li>
                             <li ><a href="#">Mitt konto</a></li>
@@ -29,11 +28,17 @@
                             <p id="provText" runat="server" style="text-align:center; font-size: 16px;">Du har ett licensieringsprov att göra.</p>
                             <asp:Button ID="startaNyttTest" runat="server" Text="Starta prov" OnClick="startaNyttTest_Click" style="margin-left: 45%; margin-bottom: 2%;"/>
                             <div id="frågeform" runat="server">
-                                <div class="fråga" runat="server">
-                                <asp:CheckBox ID="CheckBox1" runat="server" />
-                                <asp:CheckBox ID="CheckBox2" runat="server" />
-                                <asp:CheckBox ID="CheckBox3" runat="server" />
-                                <asp:CheckBox ID="CheckBox4" runat="server" />
+                                <h4 id="frågenummer" runat="server" Visible="false"></h4>
+                                <div id="fråga" runat="server">
+
+                                    </div>
+                                <div id="svarsalternativ">
+                                        <ul>
+                                            <li><asp:CheckBox ID="CheckBox1" runat="server" Visible="false" /></li>
+                                            <li><asp:CheckBox ID="CheckBox2" runat="server" Visible="false"/></li>
+                                            <li><asp:CheckBox ID="CheckBox3" runat="server" Visible="false"/></li>
+                                            <li><asp:CheckBox ID="CheckBox4" runat="server" Visible="false"/></li>
+                                        </ul>
                                     </div>
                                                             </div>
                             <asp:Button ID="nästaSida1" runat="server" Text="Nästa Fråga" OnClick="nästaSida1_Click" Visible="false" style="margin-left: 45%; margin-bottom: 2%;"/>
--- IK073G_Projektuppgift/IK073G_Projektuppgift/Nyanställd/nyanställd_Start.aspx.cs
+++ IK073G_Projektuppgift/IK073G_Projektuppgift/Nyanställd/nyanställd_Start.aspx.cs
@@ -19,7 +19,12 @@ namespace IK073G_Projektuppgift
 
         Postgres p = new Postgres();
 
-        protected void Page_Load(object sender, EventArgs e)
+
+       int resultat;
+        int fråganummer;
+
+
+    protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
@@ -43,19 +48,18 @@ namespace IK073G_Projektuppgift
 
                 HtmlGenericControl kategoriFråga = new HtmlGenericControl("p class=frågaKategori");
                 kategoriFråga.InnerText = qa.kategori;
-
-                frågeform.Controls.Add(kategoriFråga);
+                fråga.Controls.Add(kategoriFråga);
 
                 HtmlGenericControl bildFråga = new HtmlGenericControl("img src= '" + qa.bild + "' class=bildFråga width=20%");
-                frågeform.Controls.Add(bildFråga);
+                fråga.Controls.Add(bildFråga);
 
                 HtmlGenericControl rubrikFråga = new HtmlGenericControl("p class=frågaRubrik id=hej runat=server");
                 rubrikFråga.InnerText = qa.fråga;
-                frågeform.Controls.Add(rubrikFråga);
+                fråga.Controls.Add(rubrikFråga);
 
                 HtmlGenericControl divText = new HtmlGenericControl("div class=text");
                 divText.InnerText = qa.text;
-                frågeform.Controls.Add(divText);
+                fråga.Controls.Add(divText);
             }
         }
         public List<QA> XmlTillLista()
@@ -94,21 +98,38 @@ namespace IK073G_Projektuppgift
         }
         public void TaUtEnFråga()
         {
+            aktuellFråga = null;
+            CheckBox1.Checked = false;
+            CheckBox2.Checked = false;
+            CheckBox3.Checked = false;
+            CheckBox4.Checked = false;
+
+            fråganummer += 100;
+
+            Session["fråganummer"] = fråganummer;
+
+            if (Session["fråganummer"] != null)
+            {
+                fråganummer = (int)Session["fråganummer"];
+            }
+
+            frågenummer.InnerHtml = "Fråga: " + fråganummer + " av 25";
+
+
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, AllaFrågor.Count);
 
-            CheckBox1.ID = AllaFrågor[randomIndex].svar1;
             CheckBox1.Text = AllaFrågor[randomIndex].svar1;
-            CheckBox2.ID = AllaFrågor[randomIndex].svar2;
             CheckBox2.Text = AllaFrågor[randomIndex].svar2;
-            CheckBox3.ID = AllaFrågor[randomIndex].svar3;
             CheckBox3.Text = AllaFrågor[randomIndex].svar3;
-            CheckBox4.ID = AllaFrågor[randomIndex].svar4;
             CheckBox4.Text = AllaFrågor[randomIndex].svar4;
 
             aktuellFråga = AllaFrågor[randomIndex];
+            Session["AktuellFråga"] = aktuellFråga;
+
             PåbörjadFråga.Add(AllaFrågor[randomIndex]);
-            namnet.InnerHtml = randomIndex.ToString() + " " + AllaFrågor.Count.ToString() + " " + AllaFrågor[randomIndex].rättSvar1;
+            Session["PåbörjadFråga"] = PåbörjadFråga;
+            //namnet.InnerHtml = randomIndex.ToString() + " " + AllaFrågor.Count.ToString() + " " + AllaFrågor[randomIndex].rättSvar1;
             AllaFrågor.RemoveAt(randomIndex);
 
         }
@@ -124,31 +145,152 @@ namespace IK073G_Projektuppgift
             //kategori2 = false;
             //kategori3 = false;
 
+            if (Session["AktuellFråga"] != null)
+            {
+                aktuellFråga = (QA)Session["AktuellFråga"];
+            }
+
             nästaSida1.Visible = true;
+            CheckBox1.Visible = true;
+            CheckBox2.Visible = true;
+            CheckBox3.Visible = true;
+            CheckBox4.Visible = true;
+            frågenummer.Visible = true;
             //provText.Visible = false;
-            ////startaNyttTest.Visible = false;
-            //nyanställningsLista.Visible = false;
-            ////namnet.InnerHtml = nyanställningsLista.SelectedItem.Value;
+            startaNyttTest.Visible = false;
+            nyanställningsLista.Visible = false;
+            namnet.Visible = false;
+            provText.InnerHtml = "";
+
+
 
         }
 
         protected void nästaSida1_Click(object sender, EventArgs e)
         {
-            //BesvaradeFrågor.Add(PåbörjadFråga[0]);
-            PåbörjadFråga.Clear();
-            aktuellFråga = null;
+            if (Session["resultat"] != null)
+            {
+                resultat = (int)Session["resultat"];
+            }
 
-            TaUtEnFråga();
 
-            VisaAllt(PåbörjadFråga);
+            if (Session["AktuellFråga"] != null)
+            {
+                aktuellFråga = (QA)Session["AktuellFråga"];
+            }
+
 
             if (CheckBox1.Checked == true)
             {
-                hej.InnerHtml = "Rätt";
+                if (CheckBox1.Text == aktuellFråga.rättSvar1)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox1";
+                    resultat+=1;
+                }
+                else if (CheckBox1.Text == aktuellFråga.rättSvar2)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox1";
+                    resultat += 1;
+                }
+                else if (CheckBox1.Text == aktuellFråga.rättSvar3)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox1";
+                    resultat += 1;
+                }
+                else
+                {
+                    namnet.InnerHtml = "Checkbox1 är checked men inte rättsvar";
+                }
+                provText.InnerHtml = resultat.ToString();
             }
-            if (CheckBox2.Checked == true && CheckBox2.ID == aktuellFråga.rättSvar1)
+            else if (CheckBox2.Checked == true)
             {
-                hej.InnerHtml = "Rätt";
+                if (CheckBox2.Text == aktuellFråga.rättSvar1)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox2";
+                    resultat += 1;
+                }
+                else if (CheckBox2.Text == aktuellFråga.rättSvar2)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox2";
+                    resultat += 1;
+                }
+                else if (CheckBox2.Text == aktuellFråga.rättSvar3)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox2";
+                    resultat += 1;
+                }
+                else
+                {
+                    namnet.InnerHtml = "Checkbox2 är checked men inte rättsvar";
+                }
+                provText.InnerHtml = resultat.ToString();
+            }
+            else if (CheckBox3.Checked == true)
+            {
+                if (CheckBox3.Text == aktuellFråga.rättSvar1)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox3";
+                    resultat += 1;
+                }
+                else if (CheckBox3.Text == aktuellFråga.rättSvar2)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox3";
+                    resultat += 1;
+                }
+                else if (CheckBox3.Text == aktuellFråga.rättSvar3)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox3";
+                    resultat += 1;
+                }
+                else
+                {
+                    namnet.InnerHtml = "Checkbox3 är checked men inte rättsvar";
+                }
+                provText.InnerHtml = resultat.ToString();
+            }
+            else if (CheckBox4.Checked == true)
+            {
+                if (CheckBox4.Text == aktuellFråga.rättSvar1)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox4";
+                    resultat += 1;
+                }
+                else if (CheckBox4.Text == aktuellFråga.rättSvar2)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox4";
+                    resultat += 1;
+                }
+                else if (CheckBox4.Text == aktuellFråga.rättSvar3)
+                {
+                    namnet.InnerHtml = "Rätt, checkbox4";
+                    resultat += 1;
+                }
+                else
+                {
+                    namnet.InnerHtml = "Checkbox4 är checked men inte rättsvar";
+                }
+                provText.InnerHtml = resultat.ToString();
+            }
+
+            if (CheckBox1.Checked == false && CheckBox2.Checked == false && CheckBox3.Checked == false && CheckBox4.Checked == false)
+            {
+                if (Session["PåbörjadFråga"] != null)
+                {
+                    PåbörjadFråga = (List<QA>)Session["PåbörjadFråga"];
+                }
+                provText.InnerHtml = "Dra åt helvete! \n Markera ett svarsalternativ";
+                VisaAllt(PåbörjadFråga);
+            }
+            else
+            {
+                
+                Session["resultat"] = resultat;
+
+                TaUtEnFråga();
+                VisaAllt(PåbörjadFråga);
+
+
             }
 
         }
