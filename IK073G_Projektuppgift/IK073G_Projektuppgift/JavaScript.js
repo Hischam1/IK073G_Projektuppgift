cc35c2eb712cf6f975e0ad85ff4a1312e916a8de
Johan Conradsson <conradsson1993@hotmail.com>
2016-11-04 08:47:41 +0100

    lite små ändringar innan deadline

---
+++ IK073G_Projektuppgift/IK073G_Projektuppgift/JavaScript.js
@@ -0,0 +1,15 @@
+﻿///<reference path="http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js">
+
+$(document).ready(function () {
+
+    /* Gömmer allt som inte ska visas i start-up*/
+
+    $(".container").hide();
+    $("#main_väljPerson").hide();
+
+    /* Visar det som ska presenteras i start-up */
+
+    $(".container").fadeIn("slow");
+    $("#main_väljPerson").fadeIn("slow");
+});
+
