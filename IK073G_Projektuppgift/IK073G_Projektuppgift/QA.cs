98d5c76df0f985191a44d7217774678962f1895d
Johan Conradsson <conradsson1993@hotmail.com>
2017-03-17 20:51:40 +0100

    Buggfix

---
+++ IK073G_Projektuppgift/IK073G_Projektuppgift/QA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IK073G_Projektuppgift
+{
+    public class QA
+    {
+       
+        public string kategori { get; set; }
+        public string typ { get; set; }
+        public string text { get; set; }
+        public string fråga { get; set; }
+        public string bild { get; set; }
+        public string svar1 { get; set; }
+        public string svar2 { get; set; }
+        public string svar3 { get; set; }
+        public string svar4 { get; set; }
+        public string rättSvar1 { get; set; }
+        public string rättSvar2 { get; set; }
+        public string användarensSvar1 { get; set; }
+        public string användarensSvar2 { get; set; }
+        public string användarensSvar3 { get; set; }
+    }
+
+    
+}
